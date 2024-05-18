efde4ef84bdf38f14a41f1c39a63bbafa3691197
patrick-badran <patrick.badran@montyholding.com>
2024-05-18 21:28:58 +0300

    Add Cors Mappings

--- src/main/java/com/mdsl/institutionservice/dto/BaseResponse.java
+++ src/main/java/com/mdsl/institutionservice/dto/BaseResponse.java
@@ -1,19 +1,24 @@
 package com.mdsl.institutionservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
 @Accessors(chain = true)
 public class BaseResponse<T>
 {
+	@JsonIgnore
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
 	private T entity;
 	private String message;
 	private String developerMessage;
 	private int statusCode;
-	private LocalDateTime timestamp = LocalDateTime.now();
+	private String timestamp = LocalDateTime.now().format(formatter);
 }
